9a298c94b7db89975d579a539450105be041d2ae
Rohit Bhise <rohitbhise9372@gmail.com>
2026-09-11 16:08:34 +0530

    API updates

--- frontend/src/components/AllBooks.jsx
+++ frontend/src/components/AllBooks.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 
-const API = "https://books-backend-c.vercel.app/books";
+const API = "https://books-backend-c.vercel.app";
 
 export default function AllBooks() {
   const [books, setBooks] = useState([]);
